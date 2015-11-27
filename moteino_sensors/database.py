dea6e210777affb12148b0821eeece2a086c30ee
Bartosz Kupidura <bartosz.kupidura@gmail.com>
2015-11-27 11:57:59 +0100

    Add triggers for metrics

--- moteino_sensors/database.py
+++ moteino_sensors/database.py
@@ -1,6 +1,6 @@
 import time
 
-from sqlalchemy import Column, Integer, Text, Index, ForeignKey, desc, create_engine
+from sqlalchemy import Column, Integer, Text, Index, ForeignKey, desc, DDL, event, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -54,6 +54,37 @@ class Board(Base):
             self.board_desc)
 
 
+TRIGGER_SQLITE_INSERT = DDL("""
+CREATE TRIGGER insert_metric
+  INSERT ON metrics WHEN NOT EXISTS (
+    SELECT 1 FROM last_metrics WHERE board_id = new.board_id and sensor_type = new.sensor_type
+  )
+  BEGIN
+    INSERT into last_metrics VALUES (new.board_id, new.sensor_type, new.last_update, new.sensor_data);
+  END;
+""")
+
+TRIGGER_SQLITE_UPDATE = DDL("""
+CREATE TRIGGER update_metric
+  INSERT ON metrics WHEN EXISTS (
+    SELECT 1 FROM last_metrics WHERE board_id = new.board_id and sensor_type = new.sensor_type
+  )
+  BEGIN
+    UPDATE last_metrics SET sensor_data = new.sensor_data, last_update = new.last_update WHERE board_id = new.board_id and sensor_type = new.sensor_type;
+  END;
+""")
+
+event.listen(
+  Base.metadata,
+  'after_create',
+  TRIGGER_SQLITE_INSERT.execute_if(dialect='sqlite')
+)
+event.listen(
+  Base.metadata,
+  'after_create',
+  TRIGGER_SQLITE_UPDATE.execute_if(dialect='sqlite')
+)
+
 def connect(db_file):
   return create_engine('sqlite:///{}'.format(db_file))
 
@@ -87,20 +118,6 @@ def insert_metric(db, sensor_data):
   now = int(time.time())
 
   s.add(Metric(board_id = sensor_data['board_id'],
-          sensor_type = sensor_data['sensor_type'],
-          sensor_data = sensor_data['sensor_data'],
-          last_update = now))
-
-  last_metric = s.query(LastMetric).filter(
-          LastMetric.board_id == sensor_data['board_id'],
-          LastMetric.sensor_type == sensor_data['sensor_type']
-          ).first()
-
-  if last_metric:
-    last_metric.sensor_data = sensor_data['sensor_data']
-    last_metric.last_update = now
-  else:
-    s.add(LastMetric(board_id = sensor_data['board_id'],
           sensor_type = sensor_data['sensor_type'],
           sensor_data = sensor_data['sensor_data'],
           last_update = now))
