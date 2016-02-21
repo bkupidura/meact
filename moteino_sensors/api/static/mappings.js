960df4dc5b4ef76526ab5135cc69c8afe2c28591
Bartosz Kupidura <bartosz.kupidura@gmail.com>
2016-02-21 22:08:23 +0100

    Add blink command to nodes

--- moteino_sensors/api/static/mappings.js
+++ moteino_sensors/api/static/mappings.js
@@ -1,6 +1,6 @@
 commands_mapping = {
     '1': [{'name': 'measure', 'command': '%(board)s:1', 'mqtt_topic': 'srl/write'}, {'name': 'reboot', 'command': '%(board)s:255', 'mqtt_topic': 'srl/write'}],
-    'default': [{'name': 'reboot', 'command': '%(board)s:255', 'mqtt_topic': 'srl/write'}]
+    'default': [{'name': 'reboot', 'command': '%(board)s:255', 'mqtt_topic': 'srl/write'}, {'name': 'blink', 'command': '%(board)s:11', 'mqtt_topic': 'srl/write'}]
 }
 
 graphs_mapping = {
